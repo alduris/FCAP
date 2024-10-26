d68b941cfd97f4bc22e8b115cd758dfb09a47db2
alduris <152772325+alduris@users.noreply.github.com>
2024-10-26 04:58:50 -0400

    End of night karma ladder moves now

--- src/Screens/EndOfNightScreen.cs
+++ src/Screens/EndOfNightScreen.cs
@@ -6,6 +6,7 @@ namespace FCAP.Screens
     internal class EndOfNightScreen : KarmaLadderScreen
     {
         public override bool LadderInCenter => true;
+        private int moveWait = 85;
 
         public EndOfNightScreen(ProcessManager manager) : base(manager, Constants.NightOverScreen)
         {
@@ -14,10 +15,19 @@ namespace FCAP.Screens
             PlaySound(SoundID.MENU_Enter_Sleep_Screen);
         }
 
+        public override void Update()
+        {
+            base.Update();
+            if (karmaLadder != null && moveWait-- == 0)
+            {
+                karmaLadder.GoToKarma(karma.x + 1, false);
+            }
+        }
+
         public override void GetDataFromGame(SleepDeathScreenDataPackage package)
         {
             package.karma.y = 4;
-            package.karma.x = Math.Min(4, package.saveState.cycleNumber);
+            package.karma.x = Math.Min(4, package.saveState.cycleNumber - 1);
             if (package.saveState.cycleNumber <= 5)
             {
                 base.GetDataFromGame(package);
